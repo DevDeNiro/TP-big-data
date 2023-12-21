edac636fe018d523b158bcbb636fe92901a8aece
Noodles-boop <72260596+DevDeNiro@users.noreply.github.com>
2023-12-21 13:47:23 +0100

    add agregats on stream

--- Python/read_stream.py
+++ Python/read_stream.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, when
 from pyspark.sql.types import StructType, StringType, FloatType, TimestampType, IntegerType
+from pyspark.sql.functions import from_json, col, when, to_timestamp, to_date
 
 spark = SparkSession.builder \
     .appName("KafkaConsumer") \
@@ -25,13 +25,26 @@ schema = StructType().add("id_transaction", StringType()) \
 kafka_df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "127.0.0.1:9092") \
-    .option("subscribe", "transaction") \
+    .option("subscribe", "tp-big-data") \
     .load()
 
 df = kafka_df.selectExpr("CAST(value AS STRING)").select(from_json("value", schema).alias("data")).select("data.*")
-# df = df.withColumn("devise", when(col("devise") == "USD", "EUR").otherwise(col("devise")))
 
-query = df.writeStream.outputMode("append").format("parquet").option("checkpointLocation", "metadata").option("path",
-                                                                                                              "elem").start()
+# --------------------------------------------------------------------
+#                     MANIP SUR LES DF
+# --------------------------------------------------------------------
+
+# Traitement des données
+df = df.withColumn("devise", when(col("devise") == "USD", "EUR").otherwise(col("devise"))) # Conversion de la devise
+df = df.withColumn("montant_eur", col("montant") * 1.10)  # Conversion USD en EUR
+df = df.withColumn("date_timestamp", to_timestamp(col("date"), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))  # Ajouter le TimeZone
+df = df.withColumn("date", to_date(col("date")))  # Convertir la date en string en une valeur date
+df = df.filter(col("moyen_paiement") != "erreur")  # Supprimer les transactions en erreur
+df = df.filter(col("lieu").isNotNull())  # Supprimer les valeurs None pour l'adresse
+
+query = (df.writeStream.outputMode("append")
+         .format("parquet")
+         .option("checkpointLocation", "metadata")
+         .option("path", "elem").start())
 
 query.awaitTermination()
